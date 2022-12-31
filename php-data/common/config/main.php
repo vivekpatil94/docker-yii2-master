1a634d9c8abe9e050b2f4da0d04f810b4cc0c8bc
vivekpatil94 <vsoftinfotech@gmail.com>
2022-12-31 05:55:01 +0530

    Refresh project 2

--- php-data/common/config/main.php
+++ php-data/common/config/main.php
@@ -6,6 +6,13 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=db;dbname=yii2advanced',
+        'username' => 'yii2advanced',
+        'password' => 'yii2advanced',
+        'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
